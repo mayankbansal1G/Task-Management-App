d4543d9a3ca8289d46c55ec6b0c56d2317041dbd
mayank <saabbansal24@gmail.com>
2024-09-07 20:06:18 +0530

    Added Search By name api functionality and also restructured the code to add the handlers in the controller folder, Only backend till now.

---
+++ backend/controller/task.controller.js
@@ -0,0 +1,84 @@
+import Task from "../model/task.model.js";
+import mongoose from "mongoose";
+
+export const getAllTask = async (req, res)=> {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({"status": "success",data: tasks});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({"status": "error", message: `${error.message} and Cannot get all tasks`});
+    }
+    return res;
+};
+
+export const updateTask = async (req, res)=> {
+    const {taskId} = req.params;
+    const task = req.body;
+    if (!mongoose.Types.ObjectId.isValid(taskId)) {
+        return res.status(400).json({"status": "error",message: `${taskId} is not a valid task`});
+    }
+    try {
+        await Task.findByIdAndUpdate(taskId,task, {new:true});
+        res.status(200).json({"status": "success",message: "Task updated successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({"status": "error", message: `${error.message} and Cannot update task`});
+    }
+    return res;
+};
+
+export const deleteTask = async (req, res)=> {
+    const {taskId} = req.params;
+    // console.log(`Id: ${taskId}`);
+    try {
+        await Task.findByIdAndDelete(taskId);
+        res.status(200).json({"status": "success", message: "Task deleted successfully."});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({"status": "error"});
+    }
+    return res;
+};
+
+export const addTask = async (req,res)=>{
+    const task = req.body;
+    if(!task.name || !task.description || !task.taskStatus || !task.taskPriority || !task.dueDate){
+        // console.log(task);
+        return res.status(400).json({success: false, message: "Please Provide All Fields"});
+    }
+    const newTask = new Task(req.body);
+
+    try {
+        await newTask.save();
+        res.status(200).json({success: true, message: "Task Added"});
+    } catch (err) {
+        console.error("Error is creating Task")
+        console.error(err);
+        res.status(500).json({success: false, message: err.message});
+    }
+};
+
+export const testApi = (req, res)=> {
+    return res.status(200).json({"status": "success"});
+};
+
+export const getAllTasksByName = async (req,res)=>{
+    const {taskName} = req.params;
+    console.log(taskName);
+
+    const regexPattern = new RegExp(taskName, 'i'); // 'i' makes the search case-insensitive
+    try {
+        const allTasks = await Task.find(
+            {
+                name: {$regex: regexPattern}
+            }
+        );
+        res.status(200).json({success: true, data: allTasks});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: error.message});
+    }
+    return res;
+};
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,70 +1,26 @@
 import express from 'express';
+
+import {
+    addTask,
+    deleteTask,
+    getAllTask, getAllTasksByName,
+    testApi,
+    updateTask
+} from "../controller/task.controller.js";
 import Task from "../model/task.model.js";
-import mongoose from "mongoose";
 
 const taskRouter = express.Router();
 
-taskRouter.get("/test", (req, res)=> {
-    return res.status(200).json({"status": "success"});
-})
-taskRouter.get('/all', async (req, res)=> {
-    try {
-        const tasks = await Task.find({});
-        res.status(200).json({"status": "success",data: tasks});
+taskRouter.get("/test", testApi)
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({"status": "error", message: `${error.message} and Cannot get all tasks`});
-    }
-    return res;
-});
+taskRouter.post("/", addTask);
 
-taskRouter.put("/:taskId", async (req, res)=> {
-    const {taskId} = req.params;
-    const task = req.body;
-    if (!mongoose.Types.ObjectId.isValid(taskId)) {
-        return res.status(400).json({"status": "error",message: `${taskId} is not a valid task`});
-    }
-    try {
-        await Task.findByIdAndUpdate(taskId,task, {new:true});
-        res.status(200).json({"status": "success",message: "Task updated successfully"});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({"status": "error", message: `${error.message} and Cannot update task`});
-    }
-    return res;
-});
+taskRouter.get('/all', getAllTask);
 
-taskRouter.delete("/:taskId", async (req, res)=> {
-    const {taskId} = req.params;
-    // console.log(`Id: ${taskId}`);
-    try {
-        await Task.findByIdAndDelete(taskId);
-        res.status(200).json({"status": "success", message: "Task deleted successfully."});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({"status": "error"});
-    }
-    return res;
-})
+taskRouter.put("/:taskId", updateTask);
 
-taskRouter.post("/",async (req,res)=>{
-    // console.log(req);
-    const task = req.body;
-    if(!task.name || !task.description || !task.taskStatus || !task.taskPriority || !task.dueDate){
-        // console.log(task);
-        return res.status(400).json({success: false, message: "Please Provide All Fields"});
-    }
-    const newTask = new Task(req.body);
+taskRouter.delete("/:taskId", deleteTask);
 
-    try {
-        await newTask.save();
-        res.status(200).json({success: true, message: "Task Added"});
-    } catch (err) {
-        console.error("Error is creating Task")
-        console.error(err);
-        res.status(500).json({success: false, message: err.message});
-    }
-});
+taskRouter.get("/byName/:taskName", getAllTasksByName);
 
 export default taskRouter;
