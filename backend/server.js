38aa0ef4c3cd7ba4928b4cb0e6b2cdbc242b18f6
mayank <saabbansal24@gmail.com>
2024-09-10 22:06:27 +0530

    Final Edit Added Production Scripts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import {connectDB} from "./config/database.js";
 import taskRouter from "./routes/task.route.js";
 import userRoute from "./routes/user.route.js";
@@ -11,13 +12,22 @@ const app = express();
 
 app.use(express.json());
 
+const __dirname = path.resolve();
+
 app.use("/api/tasks", taskRouter);
 
 app.use("/api/users", userRoute);
 
+if (process.env.NODE_ENV !== 'development') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    )
+}
 // app.use(cors())
 
 app.listen(5001,()=>{
     connectDB();
-    console.log("Server started on port 5000...");
+    console.log("Server started on port " + 5001);
 });
